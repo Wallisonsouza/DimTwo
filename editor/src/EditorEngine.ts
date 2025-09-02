449d308f940ba50b48a1a52ccbe55980c348e7de
Wallison Souza <80233842+Wallisonsouza@users.noreply.github.com>
2025-09-02 15:42:02 -0300

    update

---
+++ editor/src/EditorEngine.ts
@@ -0,0 +1,33 @@
+import { GameEntity } from "@engine/core/base/GameEntity";
+import { EngineSystem } from "@engine/core/managers/EngineSystemManager";
+import { EngineWindow } from "@engine/core/window/EngineWindow";
+import { Engine } from "@engine/Engine";
+import { PerspectiveCamera } from "@engine/modules/3D/PerspesctiveCamera";
+
+
+export class EditorEngine extends Engine {
+    constructor() {
+        const editorWindow = new EngineWindow();
+
+        super(editorWindow);
+
+        const editorCamera = new GameEntity({
+            name: "editor_camera",
+            tag: "EditorCamera",
+        });
+
+        editorCamera.transform.position.z = 10;
+        const cameraComponent = new PerspectiveCamera({
+            entity: editorCamera
+
+        });
+
+        this.forcedCamera = cameraComponent;
+
+        this.enableSystem(EngineSystem.RenderSystem);
+        this.enableSystem(EngineSystem.EditorGizmosSystem);
+        this.enableSystem(EngineSystem.EditorTransformSystem);
+        this.enableSystem(EngineSystem.EditorFreeCameraSystem);
+
+    }
+}
